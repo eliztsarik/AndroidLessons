f8ceb4e362b14986003d2f2940acc6c2cdb0ff2d
e.tsaregorodtseva <OuSCrOUS>
2021-04-24 17:46:44 +0300

    Initial commit

---
+++ p1801constraintlayoutintro/src/main/java/com/eliztsarik/p1801constraintlayoutintro/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eliztsarik.p1801constraintlayoutintro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
